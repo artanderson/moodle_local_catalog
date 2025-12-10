db6956ae5a93f10df135b190a73a3f2e0b01afe4
Art Anderson <aanderson@aerospike.com>
2025-12-10 12:59:07 -0800

    update for compatability with Moodle 5.0

--- classes/local/hook_callbacks.php
+++ classes/local/hook_callbacks.php
@@ -3,19 +3,18 @@
 namespace local_catalog\local;
 
 use core\hook\navigation\primary_extend;
-use core\navigation\navigation_node;
 
 class hook_callbacks {
     public static function catalog_primary_extend(primary_extend $navigation) {
         $primary = $navigation->get_primaryview();
         $beforekey = $primary->get('siteadminnode') ? 'siteadminnode' : null;
         $primary->add_node(
-            new navigation_node([
+            new \navigation_node([
                 'text'=>'Catalog',
                 'shorttext'=>'Catalog',
                 'key'=> 'catalog',
                 'action'=>new \moodle_url('/local/catalog/index.php'),
-                'type'=> navigation_node::TYPE_ROOTNODE
+                'type'=> \navigation_node::TYPE_ROOTNODE
             ]),
             $beforekey
         );
