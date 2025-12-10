fbd9f2aaa03971e59977290be9b85f6572bc548e
Art Anderson <aanderson@aerospike.com>
2025-12-10 11:44:49 -0800

    remove moodle version requirement

--- version.php
+++ version.php
@@ -3,5 +3,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2025120901;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2025092600;        // Requires this Moodle version.
-$plugin->component = 'local_catalog';   // Full name of the plugin (used for diagnostics).
+$plugin->component = 'local_catalog';
+$plugin->release   = '1.0.0';
+$plugin->maturity  = MATURITY_STABLE;   // Full name of the plugin (used for diagnostics).
