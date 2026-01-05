4473908f565b15409b6213a12fdac12caf113d77
Art Anderson <aanderson@aerospike.com>
2026-01-05 11:00:22 -0800

    style fixes, bump version

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025120902;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2026010503;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->component = 'local_catalog';
 $plugin->release   = '1.0.0';
 $plugin->maturity  = MATURITY_STABLE;   // Full name of the plugin (used for diagnostics).
